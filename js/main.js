c97d9d8f13d310b23b40998e41032fbf99f9fe40
Manoj Kumar <Manoj.Kumar@Devsys130.Krossark.local>
2018-06-24 09:23:13 +0530

    Commit for the Addition of Changes : 1.UTF Meta tag and Viewport Meta tag
    CSS :
    1.Commented out unnecassary margin, padding
    2.Modified the width for various Elements
    3. Modified the font size settings for various elements.
    4. Added display :grid and display : Flex for content centering for header navigation and restaurant list
    5.set width :100 % for the elements to fill available space
    
    Signed-off-by: Manoj Kumar

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+   mapboxToken: 'pk.eyJ1IjoibWFub2prdW1hcmoiLCJhIjoiY2ppaWZhenU5MGdvbTN1cGN4MDJmd3c4YiJ9.ZNaOan0Pe_gfXWsfphJ7Zg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -163,7 +163,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -175,10 +175,11 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+ const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.addEventListener('click', () => { window.location.href = DBHelper.urlForRestaurant(restaurant); });
+  li.append(more);
+  return li
 
   return li
 }
